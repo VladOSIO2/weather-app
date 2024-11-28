744594f80bd4f02312d441f6633898dd0a6093de
VladOshovskyiTS <vladyslav.oshovskyi@tech-stack.io>
2024-11-28 09:40:52 +0200

    Add TailwindCSS

---
+++ app/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>Home</h1>
+      </main>
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </div>
+  );
+}
