c6bea7f28c263638e79b096f304b0066179fd898
VladOshovskyiTS <vladyslav.oshovskyi@tech-stack.io>
2024-11-29 17:01:04 +0200

    Implement page footer

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -9,9 +9,6 @@ export default function Home() {
           <WtHome />
         </Suspense>
       </main>
-      <footer>
-        <p>Footer</p>
-      </footer>
     </div>
   );
 }
---
+++ app/src/components/WtFooter/WtFooter.tsx
@@ -0,0 +1,9 @@
+const WtFooter = () => {
+  return (
+    <footer className="mt-auto w-full bg-gray-800 px-4 pb-12 pt-4 text-white">
+      Weather App (C) 2024, All rights could&apos;ve been reserved
+    </footer>
+  );
+};
+
+export default WtFooter;
